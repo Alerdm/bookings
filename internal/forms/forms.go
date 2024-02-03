978f30de78932df2cc2f71a1c226e12b8ca20270
Alerdm <aleroblesdm@gmail.com>
2024-02-03 04:35:34 -0300

    make-reservation form validation added

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -11,6 +11,10 @@ type Form struct {
 	Errors errors
 }
 
+// Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
 
 // New initializes a form struct
 func New(data url.Values) *Form {
@@ -25,6 +29,7 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be black")
 		return false
 	}
 	return true
